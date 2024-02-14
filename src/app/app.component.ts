9865007cf96e4e3a3744f431dc850378076c5cc8
yovert <yovertduarte51@gmail.com>
2024-02-14 08:11:04 -0500

    Pagina terminada 4pantallas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
+import { SingleComponent } from './components/single/single.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent,HomeComponent,
-  FooterComponent, AboutComponent],
+  FooterComponent, AboutComponent, SingleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ErrorComponent } from './components/error/error.component';
+import { SingleComponent } from './components/single/single.component';
 
 export const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'about', component:AboutComponent}
+  {path: 'about', component:AboutComponent},
+  {path: 'single', component:SingleComponent},
+  {path: '**', component:ErrorComponent },
 ];
